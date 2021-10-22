935b6abb5b7fe5701a6499d3ff9827a22fcf0d31
Thom <thom@pop-os.localdomain>
2021-10-22 16:21:51 +0200

    first draft of the hash functions

--- HashSet.py
+++ HashSet.py
@@ -6,6 +6,7 @@ from typing import List
 class HashSet:
     buckets: List[List] = None
     size: int = 0
+    N: int = 8
 
     def init(self):
         self.size = 0
@@ -13,37 +14,75 @@ class HashSet:
 
     # Computes hash value for a word (a string)
     def get_hash(self, word):
-        pass    # Placeholder code ==> to be replaced
+        hashed = 0
+        for i in range(len(word)):
+            hashed += ord(word[i])
+        hashed %= self.N
+        return hashed
 
     # Doubles size of bucket list
     def rehash(self):
-        pass    # Placeholder code ==> to be replaced
+        bucket = self.buckets
+        self.N = self.N * 2
+        self.buckets = []
+        self.buckets = [[] for i in range(self.N)]
+        for word in bucket:
+            self.add(word)
 
     # Adds a word to set if not already added
     def add(self, word):
-        pass    # Placeholder code ==> to be replaced
+        hashed = self.get_hash(word)
+        if word not in self.buckets[hashed]:
+            self.buckets[hashed].append(word)
+            self.size += 1
+            if self.size == self.N:
+                self.size = 0
+                self.rehash()
+        else:
+            pass
 
     # Returns a string representation of the set content
     def to_string(self):
-        pass    # Placeholder code ==> to be replaced
+        string = '{'
+        for i in self.buckets:
+            string += str(i)
+        string += '}'
+        return string
 
     # Returns current number of elements in set
     def get_size(self):
-        pass    # Placeholder code ==> to be replaced
+        n = 0
+        for bucket in self.buckets:
+            if bucket is not None:
+                n += 1
+        return n
 
     # Returns True if word in set, otherwise False
     def contains(self, word):
-        pass    # Placeholder code ==> to be replaced
+        hashed = self.get_hash(word)
+        if word not in self.buckets[hashed]:
+            return True
+        else:
+            return False
 
     # Returns current size of bucket list
     def bucket_list_size(self):
-        pass    # Placeholder code ==> to be replaced
+        size = len(self.buckets)
+        return size
 
     # Removes word from set if there, does nothing
     # if word not in set
     def remove(self, word):
-        pass    # Placeholder code ==> to be replaced
+        hashed = self.get_hash(word)
+        if word not in self.buckets[hashed]:
+            self.buckets[hashed] = []
+        else:
+            pass
 
     # Returns the size of the bucket with most elements
     def max_bucket_size(self):
-        pass    # Placeholder code ==> to be replaced
+        n = 0
+        for bucket in self.buckets:
+            if len(bucket) > n:
+                n = len(bucket)
+        return n
--- hash_main.py
+++ hash_main.py
@@ -8,11 +8,15 @@ words.init()             # Initialize with eight empty buckets
 
 # Add names to word set. Notice: a) contains duplicate names,
 # b) more than eight names ==> will trigger rehash
-names = ["Ella", "Owen", "Fred", "Zoe", "Adam", "Ceve", "Adam", "Ceve", "Jonas", "Ola", "Morgan", "Fredrik", "Simon", "Albin", "Jonas", "Amer", "David"]
+names = ["Ella", "Owen", "Fred", "Zoe", "Adam",
+         "Ceve", "Adam", "Ceve", "Jonas",
+         "Ola", "Morgan", "Fredrik", "Simon",
+         "Albin", "Jonas", "Amer", "David"]
 for name in names:
     words.add(name)
 
-print("\nto_string():", words.to_string())  # { Adam David Amer Ceve Owen Ella Jonas Morgan Fredrik Zoe Fred Albin Ola Simon }
+# Placeholder code ==> to be replaced))
+print("\nto_string():", words.to_string())
 print("get_size():", words.get_size())             # 14
 print("contains(Fred):", words.contains("Fred"))   # True
 print("contains(Bob):", words.contains("Bob"))     # False
@@ -28,4 +32,5 @@ delete = ["Ceve", "Adam", "Ceve", "Jonas", "Ola"]
 for s in delete:
     words.remove(s)
 print("\nget_size:", words.get_size())   # 10
-print("to_string():", words.to_string())   # { David Amer Owen Ella Morgan Fredrik Zoe Fred Albin Simon }
+# { David Amer Owen Ella Morgan Fredrik Zoe Fred Albin Simon }
+print("to_string():", words.to_string())
