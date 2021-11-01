480968c56d5aeadff47a83438e6063997e185937
Dingquan Miao <dm222qx@student.lnu.se>
2021-11-01 22:56:57 +0100

    Revert "Merge branch 'miao-release' into 'main'"
    This reverts merge request !8

--- bst_main.py
+++ bst_main.py
@@ -3,39 +3,35 @@ import BstMap as bst
 
 # Program starts
 # Add pairs
-# d = {"Ella": 39, "Owen": 40, "Fred": 44, "Zoe": 41, "Adam": 27, "Ceve": 37}
-d = {"Ella": 39, "Owen": 40, "Fred": 44}
+d = {"Ella": 39, "Owen": 40, "Fred": 44, "Zoe": 41, "Adam": 27, "Ceve": 37}
 map = bst.BstMap()
 for k, v in d.items():
     map.put(k, v)
-    print(map.to_string())
 print(map.to_string())        # { (Adam,27) (Ceve,37) (Ella,39) (Fred,44) (Owen,40) (Zoe,41) }
+print("Size:", map.size())    # 6
 
+# Override existing values
+print("\nOverride existing values")
+map.put("Zoe", 99)
+map.put("Ceve", 100)
+print(map.to_string())       # { (Adam,27) (Ceve,100) (Ella,39) (Fred,44) (Owen,40) (Zoe,99) }
 
-# print("Size:", map.size())    # 6
+# get
+print("\nGet(Fred):", map.get("Fred"))    # 44
+print("Get(Jonas):", map.get("Jonas"))  # None
+print("Max depth:", map.max_depth())     # 3
 
-# # Override existing values
-# print("\nOverride existing values")
-# map.put("Zoe", 99)
-# map.put("Ceve", 100)
-# print(map.to_string())       # { (Adam,27) (Ceve,100) (Ella,39) (Fred,44) (Owen,40) (Zoe,99) }
+# Check max_depth
+map.put("AA", 1)
+map.put("AAA", 2)
+map.put("AAAA", 3)
+map.put("AAAAA", 4)
 
-# # get
-# print("\nGet(Fred):", map.get("Fred"))    # 44
-# print("Get(Jonas):", map.get("Jonas"))  # None
-# print("Max depth:", map.max_depth())     # 3
+print("\nSize:", map.size())              # 10
+print("Max depth:", map.max_depth())    # 6
+print("To_string: ", map.to_string())    # { (AA,1) (AAA,2) (AAAA,3) (AAAAA,4) (Adam,27) (Ceve,100) (Ella,39) (Fred,44) (Owen,40) (Zoe,99) }
 
-# # Check max_depth
-# map.put("AA", 1)
-# map.put("AAA", 2)
-# map.put("AAAA", 3)
-# map.put("AAAAA", 4)
-
-# print("\nSize:", map.size())              # 10
-# print("Max depth:", map.max_depth())    # 6
-# print("To_string: ", map.to_string())    # { (AA,1) (AAA,2) (AAAA,3) (AAAAA,4) (Adam,27) (Ceve,100) (Ella,39) (Fred,44) (Owen,40) (Zoe,99) }
-
-# # as_list
-# lst = map.as_list()
-# print("\nList size and element type:", len(lst), type(lst[0]))  # 10 <class 'tuple'>
-# print("List content:", lst)  # [('AA', 1), ('AAA', 2), ('AAAA', 3), ('AAAAA', 4), ('Adam', 27), ('Ceve', 100), ('Ella', 39), ('Fred', 44), ('Owen', 40), ('Zoe', 99)]
+# as_list
+lst = map.as_list()
+print("\nList size and element type:", len(lst), type(lst[0]))  # 10 <class 'tuple'>
+print("List content:", lst)  # [('AA', 1), ('AAA', 2), ('AAAA', 3), ('AAAAA', 4), ('Adam', 27), ('Ceve', 100), ('Ella', 39), ('Fred', 44), ('Owen', 40), ('Zoe', 99)]
